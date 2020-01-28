4472dfeb21da64f34b1621c1783607fe1c6c0ec6
James Rogers <jarogers095@gmail.com>
2020-01-28 06:16:24 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_find(collection)
-
+  if block_given?
+    i = 0
+    while i < collection.size do
+      result = yield(collection[i])
+      if result == true
+        return collection[i]
+      else
+        i += 1
+      end
+    end
+  end
 end
