53550029b29ec5fa6a664780c58c91c96854997c
James Rogers <jarogers095@gmail.com>
2020-01-28 06:28:58 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,13 +1,14 @@
 require 'pry'
 
-def my_find(collection)
-  i = 0
-  while i < collection.size do
-    result = yield(collection[i])
-    if result == true
-      return collection[i]
-    end
-    i += 1
-  end
-  return nil
-end
+#def my_find(collection)
+#  i = 0
+#  while i < collection.size do
+#    result = yield(collection[i])
+#    if result == true
+#      return collection[i]
+#    end
+#    i += 1
+#  end
+#  return nil
+#end
+
